0ae6d44a35493dfee33bf00a721689ca10d0bbde
Evangeline Noftz <esnoftz@gmail.com>
2024-09-25 07:26:28 -0500

    Initial Commit

---
+++ ClickerGameStoreNoftz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClickerGameStoreNoftz
+//
+//  Created by EVANGELINE NOFTZ on 9/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ClickerGameStoreNoftzTests/ClickerGameStoreNoftzTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClickerGameStoreNoftzTests.swift
+//  ClickerGameStoreNoftzTests
+//
+//  Created by EVANGELINE NOFTZ on 9/25/24.
+//
+
+import Testing
+@testable import ClickerGameStoreNoftz
+
+struct ClickerGameStoreNoftzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
