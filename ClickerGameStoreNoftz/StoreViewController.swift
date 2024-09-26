7edfc10d90ea348685290ead2c3ce19fcb71b59a
esnoftz <esnoftz@gmail.com>
2024-09-26 01:11:41 -0500

    Finished project.

--- ClickerGameStoreNoftz/StoreViewController.swift
+++ ClickerGameStoreNoftz/StoreViewController.swift
@@ -9,13 +9,94 @@ import UIKit
 
 class StoreViewController: UIViewController {
 
+    @IBOutlet weak var buyWolfButtonOutlet: UIButton!
+    
+    @IBOutlet weak var buyLionButtonOutlet: UIButton!
+    
+    @IBOutlet weak var buyTigerButtonOutlet: UIButton!
+    
+    @IBOutlet weak var storeTotalPointsTextOutlet: UITextView!
+    
+    @IBOutlet weak var purchaseErrorLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        storeTotalPointsTextOutlet.text = "Total Points: \(AppData.totalClickPoints)"
+        
+        //buyWolfButtonOutlet.tintColor = UIColor.systemBrown
+        //buyLionButtonOutlet.tintColor = UIColor.systemBrown
+        //buyTigerButtonOutlet.tintColor = UIColor.systemBrown
     }
     
 
+    @IBAction func buyWolfAction(_ sender: UIButton) {
+        if AppData.wolfPurchased == true {
+            purchaseErrorLabel.text = "You already purchased the wolf!"
+        } else {
+            if AppData.totalClickPoints < 50 {
+                purchaseErrorLabel.text = "Not enough points to purchase!"
+            } else {
+                purchaseErrorLabel.text = "Wolf has been purchased!"
+                AppData.wolfPurchased = true
+                //sender.tintColor = UIColor.red
+                AppData.totalClickPoints -= 50
+                storeTotalPointsTextOutlet.text = "Total Points: \(AppData.totalClickPoints)"
+            }
+        }
+    }
+    
+    
+    @IBAction func buyLionAction(_ sender: UIButton) {
+        if AppData.lionPurchased == true {
+            purchaseErrorLabel.text = "You already purchased the lion!"
+        } else {
+            if AppData.totalClickPoints < 500 {
+                purchaseErrorLabel.text = "Not enough points to purchase!"
+            } else {
+                purchaseErrorLabel.text = "Lion has been purchased!"
+                AppData.lionPurchased = true
+                //sender.backgroundColor = UIColor.red
+                AppData.totalClickPoints -= 500
+                storeTotalPointsTextOutlet.text = "Total Points: \(AppData.totalClickPoints)"
+            }
+        }
+    }
+    
+    
+    @IBAction func buyTigerAction(_ sender: UIButton) {
+        if AppData.tigerPurchased == true {
+            purchaseErrorLabel.text = "You already purchased the tiger!"
+        } else {
+            if AppData.totalClickPoints < 2000 {
+                purchaseErrorLabel.text = "Not enough points to purchase!"
+            } else {
+                purchaseErrorLabel.text = "Tiger has been purchased!"
+                AppData.tigerPurchased = true
+                //sender.backgroundColor = UIColor.red
+                AppData.totalClickPoints -= 2000
+                storeTotalPointsTextOutlet.text = "Total Points: \(AppData.totalClickPoints)"
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
--- ClickerGameStoreNoftz/ViewController.swift
+++ ClickerGameStoreNoftz/ViewController.swift
@@ -5,6 +5,14 @@
 //  Created by EVANGELINE NOFTZ on 9/25/24.
 //
 
+class AppData {
+    static var totalClickPoints = 0
+    static var wolfPurchased = false
+    static var lionPurchased = false
+    static var tigerPurchased = false
+}
+
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -23,39 +31,80 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var goToStoreButtonOutlet: UIButton!
     
+    var defaultImage: UIImage!
     
-    var totalClickPoints = 0
+    var wolfOn = false
+    var lionOn = false
+    var tigerOn = false
+    
+    //var totalClickPoints = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        pointMenuTextViewOutlet.text = "*These are the point values of the current animals you have!*\n\nFox: 1 point"
+        pointMenuTextViewOutlet.text = "*These are the point values of the current animals you have!*\n\n\nFox: 1 point"
+        
+        defaultImage = animal4ImageOutlet.image!
         
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        print("home screen appearing")
+        mainTotalPointsViewOutlet.text = "Total Points: \(AppData.totalClickPoints)"
+        if AppData.wolfPurchased == true && wolfOn == false {
+            animal2ImageOutlet.image = UIImage(named: "Wolf Image")
+            pointMenuTextViewOutlet.text += "\nWolf: 5 points"
+            wolfOn = true
+        }
+        
+        if AppData.lionPurchased == true && lionOn == false {
+            animal3ImageOutlet.image = UIImage(named: "Lion Image2")
+            pointMenuTextViewOutlet.text += "\nLion: 10 points"
+            lionOn = true
+        }
+        
+        if AppData.tigerPurchased == true && tigerOn == false {
+            animal4ImageOutlet.image = UIImage(named: "Tiger Image3")
+            pointMenuTextViewOutlet.text += "\nTiger: 15 points"
+            tigerOn = true
+            print("showing tiger")
+        }
+        
+        
+    }
+    
     
     // Lower right
     @IBAction func animal1TapAction(_ sender: UITapGestureRecognizer) {
-        print("tap 1")
-        totalClickPoints += 15
-        mainTotalPointsViewOutlet.text = "Total Points: \(totalClickPoints)"
+        print("tap 1 animal 4 (tiger)")
+        //print(animal4ImageOutlet.imageNa)
+        if animal4ImageOutlet.image != defaultImage {
+            AppData.totalClickPoints += 15
+            mainTotalPointsViewOutlet.text = "Total Points: \(AppData.totalClickPoints)"
+        }
+        //if animal1ImageOutlet.image == UII
     }
     
     
     // Lower left
     @IBAction func animal2TapAction(_ sender: UITapGestureRecognizer) {
-        print("tap 2")
-        totalClickPoints += 10
-        mainTotalPointsViewOutlet.text = "Total Points: \(totalClickPoints)"
+        print("tap 2 animal 3 (lion)")
+        if animal3ImageOutlet.image != defaultImage {
+            AppData.totalClickPoints += 10
+            mainTotalPointsViewOutlet.text = "Total Points: \(AppData.totalClickPoints)"
+        }
     }
     
     
     // Upper right
     @IBAction func animal3TapAction(_ sender: UITapGestureRecognizer) {
-        print("tap 3")
-        totalClickPoints += 5
-        mainTotalPointsViewOutlet.text = "Total Points: \(totalClickPoints)"
+        print("tap 3 animal 2 (wolf)")
+        if animal2ImageOutlet.image != defaultImage {
+            AppData.totalClickPoints += 5
+            mainTotalPointsViewOutlet.text = "Total Points: \(AppData.totalClickPoints)"
+        }
+        
     }
     
     
@@ -63,9 +112,10 @@ class ViewController: UIViewController {
     
     // Upper left
     @IBAction func animal4TapAction(_ sender: UITapGestureRecognizer) {
-        print("tap 4")
-        totalClickPoints += 1
-        mainTotalPointsViewOutlet.text = "Total Points: \(totalClickPoints)"
+        print("tap 4 animal 1 (fox)")
+        AppData.totalClickPoints += 1
+        mainTotalPointsViewOutlet.text = "Total Points: \(AppData.totalClickPoints)"
+        
     }
     
     
